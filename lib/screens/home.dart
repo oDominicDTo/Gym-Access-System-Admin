1e70e85a331c0ab23b1dc391c3e7f856d44e0a04
Cee <annecrisel0@gmail.com>
2023-12-11 20:31:12 +0800

    Interface and Menu Options

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,28 @@
+// main.dart
+
 import 'package:flutter/material.dart';
+import 'package:gym_kiosk_admin/widget/add_new_member_form.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-import '../widget/custom_drawer.dart';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeAdmin(),
+    );
+  }
+}
 
+class HomeAdmin extends StatefulWidget {
+  @override
+  _HomeAdminState createState() => _HomeAdminState();
+}
 
-
-class HomeAdmin extends StatelessWidget {
-  TextEditingController _searchController = TextEditingController();
-
+class _HomeAdminState extends State<HomeAdmin> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +36,12 @@ class HomeAdmin extends StatelessWidget {
           title: Row(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 30.0, bottom: 25.0, left: 2.0, right: 2.0),
+                padding: const EdgeInsets.only(
+                  top: 30.0,
+                  bottom: 25.0,
+                  left: 2.0,
+                  right: 2.0,
+                ),
                 child: SizedBox(
                   height: 50,
                   width: 300,
@@ -30,76 +50,95 @@ class HomeAdmin extends StatelessWidget {
                     fit: BoxFit.contain,
                   ),
                 ),
-              ),
 
+              ),
             ],
           ),
           actions: [
-            Stack(
-              children: [
-                Container(
-                  height: 50,
-                  width: 300,
-                  margin: EdgeInsets.all(8),
-                  child: TextField(
-                    controller: _searchController,
-                    onChanged: (value) {
-                      // Handle the search text changes here
-                    },
-                    decoration: InputDecoration(
-                        hintText: 'Search',
-                        hintStyle: TextStyle(fontFamily: 'Poppins'),
-                      border: OutlineInputBorder(),
-                      prefixIcon: _searchController.text.isNotEmpty
-                          ? IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: () {
-                          _searchController.clear();
-                          // Add logic to clear the search field
-                        },
-                      )
-                          : null,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 10,
-                  top: 5,
-                  child: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {
-                      _searchController.clear();
-                      // Add logic to clear the search field
-                    },
-                  ),
-                ),
-              ],
-            ),
-            // Add New Member Button
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:  ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                onPressed: () {
-
-
-                  // Save logic here
-                },
-                child: Text(
-                  'Add Member',
-                  style: TextStyle(fontFamily: 'Poppins', color: Colors.white),
-                ),
-              ),
-            ),
+           // Display the search bar in the AppBar
           ],
         ),
       ),
-      drawer: CustomDrawer(),
-      body: Center(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home_filled),
+                label: Text('Home',
+                  style: TextStyle(fontFamily: 'Poppins'),
+                ),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.add_box_rounded),
+                label: Text('Add',
+                  style: TextStyle(fontFamily: 'Poppins'),),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bar_chart),
+                label: Text('Overview',
+                  style: TextStyle(fontFamily: 'Poppins'),),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.stars_rounded),
+                label: Text('Membership',
+                  style: TextStyle(fontFamily: 'Poppins'),),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.person),
+                label: Text('Profile',
+                  style: TextStyle(fontFamily: 'Poppins'),),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Log Out',
+                  style: TextStyle(fontFamily: 'Poppins'),),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          // Content displayed on the right side based on the selected index
+          Expanded(
+            child: Center(
+              child: _getContent(_selectedIndex),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (
 
-          child: Text('Renew Subscription'),
-
+            ) {
+          // Add your FloatingActionButton logic here
+        },
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  Widget _getContent(int index) {
+    switch (index) {
+      case 0:
+        return Center(child: Text('Home Content'));
+      case 1:
+        return AddNewMemberForm();
+      case 2:
+        return Center(child: Text('3'));
+      case 4:
+        return Center(child: Text('4'));
+      case 5:
+        return Center(child: Text('5'));
+      case 6:
+        return Center(child: Text('6'));
+      default:
+        return Center(child: Text('Invalid Content'));
+    }
   }
 }
